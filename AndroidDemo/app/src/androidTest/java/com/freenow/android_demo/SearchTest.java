cf34b8d175ba6f8bd04e0490b8f8f8f913aba867
Duygu Onaral <donaral@protel.com.tr>
2019-11-18 17:22:20 +0300

    inRoot RootMatchers isPlatformPopup did not work

--- AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/SearchTest.java
+++ AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/SearchTest.java
@@ -1,5 +1,6 @@
 package com.freenow.android_demo;
 
+import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,11 +10,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -30,6 +34,10 @@ public class SearchTest {
 
         onView(withId(R.id.textSearch))
                 .perform(typeText("sa"));
+
+        onData(withText("Samantha Reed"))
+                .inRoot(RootMatchers.isPlatformPopup())
+                .perform(click());
 
     }
 

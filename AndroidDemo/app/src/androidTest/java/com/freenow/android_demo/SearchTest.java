cb78cc377519784055ae173bf6e4bfe8eebee862
Duygu Onaral <donaral@protel.com.tr>
2019-11-22 17:02:12 +0300

    before after hook in test to register and unregister idlingResource

--- AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/SearchTest.java
+++ AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/SearchTest.java
@@ -1,14 +1,20 @@
 package com.freenow.android_demo;
-import android.os.SystemClock;
+
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
 import com.freenow.android_demo.activities.MainActivity;
-import org.hamcrest.Matchers;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -16,11 +22,27 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 public class SearchTest {
+
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    private IdlingResource idlingResource;
+
+    @Before
+    public void register() {
+        idlingResource = new AutocompleteShowIdlingResource(mainActivityTestRule.getActivity());
+        IdlingRegistry.getInstance().register(idlingResource);
+    }
+
+    @After
+    public void unregister() {
+        IdlingRegistry.getInstance().unregister(idlingResource);
+    }
+
     @Test
     public void searchForDriver() {
         onView(withId(R.id.nav_username))
@@ -30,14 +52,9 @@ public class SearchTest {
                 .perform(typeText("sa"))
                 .perform(closeSoftKeyboard());
 
-        SystemClock.sleep(3000);
-
+        //that's where Espresso will wait until the idling resource is idle
         onView(withText("Samantha Reed"))
                 .inRoot(RootMatchers.isPlatformPopup())
-                .perform(click());
-
-        onView(withText("Samantha Reed"))
-                .inRoot(RootMatchers.withDecorView(Matchers.not(mainActivityTestRule.getActivity().getWindow().getDecorView())))
                 .check(matches(ViewMatchers.isDisplayed()))
                 .perform(click());
     }
